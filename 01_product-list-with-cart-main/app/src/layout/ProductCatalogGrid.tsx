8fc7597dc414ea2355fee9a89e7f6a0aa560d231
Brian L Ellis <brian@briansresume.com>
2025-08-27 18:53:23 -0400

    cart button

--- 01_product-list-with-cart-main/app/src/layout/ProductCatalogGrid.tsx
+++ 01_product-list-with-cart-main/app/src/layout/ProductCatalogGrid.tsx
@@ -1,4 +1,6 @@
+import { cn } from "@/lib/utils"
 import { useEffect, useState } from 'react'
+import { ShoppingCart } from 'lucide-react'
 
 
 function ProductAmountSelector() {
@@ -7,18 +9,36 @@ function ProductAmountSelector() {
 }
 
 function ProductAddToCart() {
-  return (<div className="w-full">
-  </div>)
+  const ICON_SZ = 14
+
+  return (
+      <div 
+        className={cn(
+          "w-[150px] p-2 text-orange-700 bg-white",
+          "border-1 rounded-full border-orange-700",
+          `absolute left-0 right-0 mx-auto bottom-[-${ICON_SZ}px]`
+        )}
+      >
+        <div className="flex items-center m-auto w-[100px]">
+          <ShoppingCart size={ICON_SZ} />
+          <span className="ml-2 text-sm">Add to Cart</span>
+        </div>
+      </div>
+  )
 }
 
 
 function ProductCard({ product }) {
-  return (<div className="w-full">
-    <img 
-      src={`/images/${product.image.desktop}`} 
-      alt={product.name}
-      className="w-full h-48 object-cover rounded-lg"
-    />
+  return (<div className="w-full text-center">
+    <div className="relative">
+      <img 
+        src={`/images/${product.image.desktop}`} 
+        alt={product.name}
+        className="w-full h-48 rounded-lg mb-[40px]"
+      />
+      {product.id ? <ProductAmountSelector /> : <ProductAddToCart />}
+    </div>
+
     <div className="text-left">
       <h3>{product.category}</h3>
       <h4 className="">{product.name}</h4>
@@ -46,10 +66,7 @@ export function ProductCatalogGrid({
 
   return (<div className={`grid ${cssColumn}`}>
     {products.map((prod, i) => {
-      return (<div key={`prod-grid-${i}`}>
-        <ProductCard product={prod} />
-        {prod.id ? <ProductAmountSelector /> : <ProductAddToCart />}
-      </div>) 
+      return (<ProductCard key={`prod-grid-${i}`} product={prod} />) 
     })}
   </div>)
 }
